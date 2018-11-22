0a33497f7590d95bc7149edf51a0706f0ed2e866
Teh64 <teh.picix@gmail.com>
2018-11-22 14:25:51 -0500

    add Autolink component and

--- src/photon.js
+++ src/photon.js
@@ -4,6 +4,7 @@ x=w-x;y=h/2-y;f=(x*dx+y*dy+((h+w)/2)*dj)/d;if(a.hasClass("waves-ink")){return 0.
 return f};var scale='scale('+wavesfn(element,e)+')';var translate='translate(0,0)';if(velocity){translate='translate('+(velocity.x)+'px, '+(velocity.y)+'px)'}ripple.setAttribute('data-hold',Date.now());ripple.setAttribute('data-x',relativeX);ripple.setAttribute('data-y',relativeY);ripple.setAttribute('data-scale',scale);ripple.setAttribute('data-translate',translate);var rippleStyle={top:relativeY+'px',left:relativeX+'px'};ripple.classList.add('waves-notransition');ripple.setAttribute('style',convertStyle(rippleStyle));ripple.classList.remove('waves-notransition');rippleStyle['-webkit-transform']=scale+' '+translate;rippleStyle['-moz-transform']=scale+' '+translate;rippleStyle['-ms-transform']=scale+' '+translate;rippleStyle['-o-transform']=scale+' '+translate;rippleStyle.transform=scale+' '+translate;var duration=e.type==='mousemove'?750:Effect.duration;rippleStyle['-webkit-transition-duration']=duration+'ms';rippleStyle['-moz-transition-duration']=duration+'ms';rippleStyle['-o-transition-duration']=duration+'ms';rippleStyle['transition-duration']=duration+'ms';ripple.setAttribute('style',convertStyle(rippleStyle))},hide:function(e,element){element=element||this;var ripples=element.getElementsByClassName('waves-rippling');for(var i=0,len=ripples.length;i<len;i++){removeRipple(e,element,ripples[i])}if(isTouchAvailable){element.removeEventListener('touchend',Effect.hide);element.removeEventListener('touchcancel',Effect.hide)}element.removeEventListener('mouseup',Effect.hide);element.removeEventListener('mouseleave',Effect.hide)}};var TagWrapper={input:function(element){var parent=element.parentNode;if(parent.tagName.toLowerCase()==='i'&&parent.classList.contains('waves-effect')){return}var wrapper=document.createElement('i');wrapper.className=element.className+' waves-input-wrapper';element.className='waves-button-input';parent.replaceChild(wrapper,element);wrapper.appendChild(element);var elementStyle=window.getComputedStyle(element,null);var color=elementStyle.color;var backgroundColor=elementStyle.backgroundColor;wrapper.setAttribute('style','color:'+color+';background:'+backgroundColor);element.setAttribute('style','background-color:rgba(0,0,0,0);')},img:function(element){var parent=element.parentNode;if(parent.tagName.toLowerCase()==='i'&&parent.classList.contains('waves-effect')){return}var wrapper=document.createElement('i');parent.replaceChild(wrapper,element);wrapper.appendChild(element)}};function removeRipple(e,el,ripple){if(!ripple){return}ripple.classList.remove('waves-rippling');var relativeX=ripple.getAttribute('data-x');var relativeY=ripple.getAttribute('data-y');var scale=ripple.getAttribute('data-scale');var translate=ripple.getAttribute('data-translate');var diff=Date.now()-Number(ripple.getAttribute('data-hold'));var delay=0;if(e.type==='mousemove'){delay=150}var duration=e.type==='mousemove'?750:Effect.duration;setTimeout(function(){var style={top:relativeY+'px',left:relativeX+'px',opacity:'0','-webkit-transition-duration':duration+'ms','-moz-transition-duration':duration+'ms','-o-transition-duration':duration+'ms','transition-duration':duration+'ms','-webkit-transform':scale+' '+translate,'-moz-transform':scale+' '+translate,'-ms-transform':scale+' '+translate,'-o-transform':scale+' '+translate,'transform':scale+' '+translate};ripple.setAttribute('style',convertStyle(style));setTimeout(function(){try{el.removeChild(ripple)}catch(e){return!1}},duration)},delay)}var TouchHandler={touches:0,allowEvent:function(e){var allow=!0;if(/^(mousedown|mousemove)$/.test(e.type)&&TouchHandler.touches){allow=!1}return allow},registerEvent:function(e){var eType=e.type;if(eType==='touchstart'){TouchHandler.touches+=1}else if(/^(touchend|touchcancel)$/.test(eType)){setTimeout(function(){if(TouchHandler.touches){TouchHandler.touches-=1}},500)}}};function getWavesEffectElement(e){if(TouchHandler.allowEvent(e)===!1){return null}var element=null;var target=e.target||e.srcElement;while(target.parentElement){if((!(target instanceof SVGElement))&&target.classList.contains('waves-effect')){element=target;break}target=target.parentElement}return element}function showEffect(e){var element=getWavesEffectElement(e);if(element!==null){if(element.disabled||element.getAttribute('disabled')||element.classList.contains('disabled')){return}TouchHandler.registerEvent(e);if(e.type==='touchstart'&&Effect.delay){var hidden=!1;var timer=setTimeout(function(){timer=null;Effect.show(e,element)},Effect.delay);var hideEffect=function(hideEvent){if(timer){clearTimeout(timer);timer=null;Effect.show(e,element)}if(!hidden){hidden=!0;Effect.hide(hideEvent,element)}removeListeners()};var touchMove=function(moveEvent){if(timer){clearTimeout(timer);timer=null}hideEffect(moveEvent);removeListeners()};element.addEventListener('touchmove',touchMove,!1);element.addEventListener('touchend',hideEffect,!1);element.addEventListener('touchcancel',hideEffect,!1);var removeListeners=function(){element.removeEventListener('touchmove',touchMove);element.removeEventListener('touchend',hideEffect);element.removeEventListener('touchcancel',hideEffect)}}else{Effect.show(e,element);if(isTouchAvailable){element.addEventListener('touchend',Effect.hide,!1);element.addEventListener('touchcancel',Effect.hide,!1)}element.addEventListener('mouseup',Effect.hide,!1);element.addEventListener('mouseleave',Effect.hide,!1)}}}Waves.init=function(options){var body=document.body;options=options||{};if('duration' in options){Effect.duration=options.duration}if('delay' in options){Effect.delay=options.delay}if(isTouchAvailable){body.addEventListener('touchstart',showEffect,!1);body.addEventListener('touchcancel',TouchHandler.registerEvent,!1);body.addEventListener('touchend',TouchHandler.registerEvent,!1)}body.addEventListener('mousedown',showEffect,!1)};Waves.attach=function(elements,classes){elements=getWavesElements(elements);if(toString.call(classes)==='[object Array]'){classes=classes.join(' ')};classes=classes?' '+classes:'';var element,tagName;for(var i=0,len=elements.length;i<len;i++){element=elements[i];tagName=element.tagName.toLowerCase();if(['input','img'].indexOf(tagName)!==-1){TagWrapper[tagName](element);element=element.parentElement}if(element.className.indexOf('waves-effect')===-1){element.className+=' waves-effect'+classes}}};Waves.ripple=function(elements,options){elements=getWavesElements(elements);var elementsLen=elements.length;options=options||{};options.wait=options.wait||0;options.position=options.position||null;if(elementsLen){var element,pos,off,centre={},i=0;var mousedown={type:'mousedown',button:1};var hideRipple=function(mouseup,element){return function(){Effect.hide(mouseup,element)}};for(;i<elementsLen;i++){element=elements[i];pos=options.position||{x:element.clientWidth/2,y:element.clientHeight/2};off=offset(element);centre.x=off.left+pos.x;centre.y=off.top+pos.y;mousedown.pageX=centre.x;mousedown.pageY=centre.y;Effect.show(mousedown,element);if(options.wait>=0&&options.wait!==null){var mouseup={type:'mouseup',button:1};setTimeout(hideRipple(mouseup,element),options.wait)}}}};Waves.calm=function(elements){elements=getWavesElements(elements);var mouseup={type:'mouseup',button:1};for(var i=0,len=elements.length;i<len;i++){Effect.hide(mouseup,elements[i])}};Waves.displayEffect=function(options){Waves.init(options)};return Waves};$(()=>{$(".waves-effect").on("touchstart",function(e){var x=e.targetTouches[0].pageX-$(this).offset().left;var y=e.targetTouches[0].pageY-$(this).offset().top;Waves.ripple(this,{wait:1e10,position:$(this).hasClass("waves-ink")?null:{x:x,y:y}})}).on("touchend",function(e){Waves.calm(this)})});if(typeof define==='function'&&define.amd){define([],function(){window.Waves=factory.call(window);return window.Waves})}else if(typeof exports==='object'){module.exports=factory.call(window)}else{window.Waves=factory.call(window)}})(window);var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||!1;descriptor.configurable=!0;if("value"in descriptor)descriptor.writable=!0;Object.defineProperty(target,descriptor.key,descriptor)}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor}}();function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function")}}
 
 var Photon = {
+	autoready: true,
 	theme: {
 		colors: {"red":{"50":"#ffebee","100":"#ffcdd2","200":"#ef9a9a","300":"#e57373","400":"#ef5350","500":"#f44336","600":"#e53935","700":"#d32f2f","800":"#c62828","900":"#b71c1c","a100":"#ff8a80","a200":"#ff5252","a400":"#ff1744","a700":"#d50000"},"pink":{"50":"#fce4ec","100":"#f8bbd0","200":"#f48fb1","300":"#f06292","400":"#ec407a","500":"#e91e63","600":"#d81b60","700":"#c2185b","800":"#ad1457","900":"#880e4f","a100":"#ff80ab","a200":"#ff4081","a400":"#f50057","a700":"#c51162"},"purple":{"50":"#f3e5f5","100":"#e1bee7","200":"#ce93d8","300":"#ba68c8","400":"#ab47bc","500":"#9c27b0","600":"#8e24aa","700":"#7b1fa2","800":"#6a1b9a","900":"#4a148c","a100":"#ea80fc","a200":"#e040fb","a400":"#d500f9","a700":"#aa00ff"},"deeppurple":{"50":"#ede7f6","100":"#d1c4e9","200":"#b39ddb","300":"#9575cd","400":"#7e57c2","500":"#673ab7","600":"#5e35b1","700":"#512da8","800":"#4527a0","900":"#311b92","a100":"#b388ff","a200":"#7c4dff","a400":"#651fff","a700":"#6200ea"},"indigo":{"50":"#e8eaf6","100":"#c5cae9","200":"#9fa8da","300":"#7986cb","400":"#5c6bc0","500":"#3f51b5","600":"#3949ab","700":"#303f9f","800":"#283593","900":"#1a237e","a100":"#8c9eff","a200":"#536dfe","a400":"#3d5afe","a700":"#304ffe"},"blue":{"50":"#e3f2fd","100":"#bbdefb","200":"#90caf9","300":"#64b5f6","400":"#42a5f5","500":"#2196f3","600":"#1e88e5","700":"#1976d2","800":"#1565c0","900":"#0d47a1","a100":"#82b1ff","a200":"#448aff","a400":"#2979ff","a700":"#2962ff"},"lightblue":{"50":"#e1f5fe","100":"#b3e5fc","200":"#81d4fa","300":"#4fc3f7","400":"#29b6f6","500":"#03a9f4","600":"#039be5","700":"#0288d1","800":"#0277bd","900":"#01579b","a100":"#80d8ff","a200":"#40c4ff","a400":"#00b0ff","a700":"#0091ea"},"cyan":{"50":"#e0f7fa","100":"#b2ebf2","200":"#80deea","300":"#4dd0e1","400":"#26c6da","500":"#00bcd4","600":"#00acc1","700":"#0097a7","800":"#00838f","900":"#006064","a100":"#84ffff","a200":"#18ffff","a400":"#00e5ff","a700":"#00b8d4"},"teal":{"50":"#e0f2f1","100":"#b2dfdb","200":"#80cbc4","300":"#4db6ac","400":"#26a69a","500":"#009688","600":"#00897b","700":"#00796b","800":"#00695c","900":"#004d40","a100":"#a7ffeb","a200":"#64ffda","a400":"#1de9b6","a700":"#00bfa5"},"green":{"50":"#e8f5e9","100":"#c8e6c9","200":"#a5d6a7","300":"#81c784","400":"#66bb6a","500":"#4caf50","600":"#43a047","700":"#388e3c","800":"#2e7d32","900":"#1b5e20","a100":"#b9f6ca","a200":"#69f0ae","a400":"#00e676","a700":"#00c853"},"lightgreen":{"50":"#f1f8e9","100":"#dcedc8","200":"#c5e1a5","300":"#aed581","400":"#9ccc65","500":"#8bc34a","600":"#7cb342","700":"#689f38","800":"#558b2f","900":"#33691e","a100":"#ccff90","a200":"#b2ff59","a400":"#76ff03","a700":"#64dd17"},"lime":{"50":"#f9fbe7","100":"#f0f4c3","200":"#e6ee9c","300":"#dce775","400":"#d4e157","500":"#cddc39","600":"#c0ca33","700":"#afb42b","800":"#9e9d24","900":"#827717","a100":"#f4ff81","a200":"#eeff41","a400":"#c6ff00","a700":"#aeea00"},"yellow":{"50":"#fffde7","100":"#fff9c4","200":"#fff59d","300":"#fff176","400":"#ffee58","500":"#ffeb3b","600":"#fdd835","700":"#fbc02d","800":"#f9a825","900":"#f57f17","a100":"#ffff8d","a200":"#ffff00","a400":"#ffea00","a700":"#ffd600"},"amber":{"50":"#fff8e1","100":"#ffecb3","200":"#ffe082","300":"#ffd54f","400":"#ffca28","500":"#ffc107","600":"#ffb300","700":"#ffa000","800":"#ff8f00","900":"#ff6f00","a100":"#ffe57f","a200":"#ffd740","a400":"#ffc400","a700":"#ffab00"},"orange":{"50":"#fff3e0","100":"#ffe0b2","200":"#ffcc80","300":"#ffb74d","400":"#ffa726","500":"#ff9800","600":"#fb8c00","700":"#f57c00","800":"#ef6c00","900":"#e65100","a100":"#ffd180","a200":"#ffab40","a400":"#ff9100","a700":"#ff6d00"},"deeporange":{"50":"#fbe9e7","100":"#ffccbc","200":"#ffab91","300":"#ff8a65","400":"#ff7043","500":"#ff5722","600":"#f4511e","700":"#e64a19","800":"#d84315","900":"#bf360c","a100":"#ff9e80","a200":"#ff6e40","a400":"#ff3d00","a700":"#dd2c00"},"brown":{"50":"#efebe9","100":"#d7ccc8","200":"#bcaaa4","300":"#a1887f","400":"#8d6e63","500":"#795548","600":"#6d4c41","700":"#5d4037","800":"#4e342e","900":"#3e2723"},"grey":{"50":"#fafafa","100":"#f5f5f5","200":"#eeeeee","300":"#e0e0e0","400":"#bdbdbd","500":"#9e9e9e","600":"#757575","700":"#616161","800":"#424242","900":"#212121"},"bluegrey":{"50":"#eceff1","100":"#cfd8dc","200":"#b0bec5","300":"#90a4ae","400":"#78909c","500":"#607d8b","600":"#546e7a","700":"#455a64","800":"#37474f","900":"#263238"}},
 		setBarColor: function(c) {
@@ -25,17 +26,13 @@ var Photon = {
 				$(this).children("input").addClass("containscontent")
 			}
 		});
-		$(".input-field.backboard").each(function(){
-			if($(this).children("label").length == 1) $(this).addClass("containslabel")
-		})
 	}
 }
 
-disableArrowKeyScrolling = false;
-
+Photon.disableArrowKeyScrolling = false;
 window.addEventListener("keydown", function(e) {
     if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-        if(disableArrowKeyScrolling){
+        if(Photon.disableArrowKeyScrolling){
 			e.preventDefault();
 		}
     }
@@ -111,6 +108,48 @@ window.addEventListener("keydown", function(e) {
 
 		return this;
 	}
+	$.fn.autolink = function(){
+		this.each(function(){
+			if(!$(this).hasClass("photon-init")){
+				const href = $(this).attr("href");
+				const content = $(this).text();
+				const link = $(this);
+
+				link.html(`<div class="progress"><div class="indeterminate"></div></div>`);
+				link.addClass("photon-init");
+
+				$.ajax({
+				    url: "https://photoncss.herokuapp.com/service/api/pageinfo",
+					type:"POST",
+					data:{
+						"url":href
+					},
+				    success: function(data) {
+						if(data.error) {
+							link.html(`<div class="progress offl"><div class="offline"></div></div>`);
+							return;
+						}
+
+				        link.html(`<div class="padding-layer"><div class="external-img"><img src="${data.icon}" alt=""/></div><div class="title">${data.title}</div><p>${content}</p><i class="material-icons waves-effect waves-ink">launch</i><div class="ref">${link.data("ref") || ""}</div></div>`);
+						link.click(function(e){
+							if($(e.target).hasClass("material-icons")){
+								e.preventDefault();
+								window.open(href);
+							}
+						});
+						link.children(".padding-layer").children(".material-icons").tooltip({
+							"tooltip":"Open in new window",
+							"position":"bottom"
+						});
+						Photon.Waves.reload();
+				    },
+					error: function(){
+						link.html(`<div class="progress offl"><div class="offline"></div></div>`);
+					}
+				});
+			}
+		})
+	};
 	$.fn.collapsible = function(options, methodParam) {
 		var defaults = {
 			accordion: undefined,
@@ -307,10 +346,10 @@ window.addEventListener("keydown", function(e) {
 			})
 			input.focus(function(){
 				acopt.addClass("active");
-				disableArrowKeyScrolling = true;
+				Photon.disableArrowKeyScrolling = true;
 				$(this).keyup();
 			}).blur(function(){
-				disableArrowKeyScrolling = false;
+				Photon.disableArrowKeyScrolling = false;
 			}).click(function(e){
 				e.stopPropagation();
 			}).keydown(function(e){
@@ -2181,9 +2220,32 @@ window.addEventListener("keydown", function(e) {
 
 }())
 
+Photon.Waves = {reload:() => {
+	$(".waves-ink").off("mousedown").bind("mousedown", function(e) {
+		$(this).children(".waves-ripple").fadeOut(500);
+		e.stopPropagation();
+		Waves.ripple(this, {
+			wait: 1e10
+		});
+	}).on("mouseup mouseleave", function() {
+		Waves.calm(this);
+	}).bind("touchstart", function(e) {
+		e.stopPropagation()
+	})
+}};
+
+$(() => {
+	$(document.body).mousemove(function(e){
+		window.cursor = {};
+		window.cursor.X = e.pageX;
+		window.cursor.Y = e.pageY;
+	});
+})
+
 Photon.ready = Photon.reload = function() {
 
 	$(".autocomplete").autocomplete();
+	$(".autolink").autolink();
 	$(".collapsible").collapsible();
 	$(".scrollnav").scrollnav();
 	$(".select").select();
@@ -2195,17 +2257,9 @@ Photon.ready = Photon.reload = function() {
 
 	Waves.init();
 
-	setInterval(() => {
-		$(document.body).mousemove(function(e){
-			window.cursor = {};
-		    window.cursor.X = e.pageX;
-		    window.cursor.Y = e.pageY;
-		})
-	})
-
 	$("iframe[src^='https://youtube.com/embed'],iframe[src^='http://youtube.com/embed']").each(function() {
 		$(this).attr("height", $(this).width() * (9 / 16));
-	})
+	});
 
 	$("label").each(function() {
 		var f = $("#" + $(this).attr("for"));
@@ -2224,20 +2278,28 @@ Photon.ready = Photon.reload = function() {
 		$(this).siblings("input").prop("checked",!$(this).siblings("input").prop("checked")).change()
 	})
 
-	$(".input-field input,.input-field.select select").on("focus", function(e) {
+	$(".input-field input,.input-field.select select").focus(function(e) {
 		$(this).parent().append("<div class=\"bar\"></div>");
 		var bar = $(this).siblings(".bar");
+
+		var {X} = window.cursor;
+		X = X - $(this).offset().left;
+		X = X/$(this).width() * 100;
+
+		bar.css({
+			left:X + "%"
+		})
 
 		bar.animate({
 			left: 0,
 			width: "100%"
 		}, Photon.speed, "swing");
 	}).blur(function() {
-		$(this).siblings(".bar").animate({
-			width: 0,
-			left: "50%"
-		}, Photon.speed, "swing", function() {
-			$(this).remove();
+		const b = $(this).siblings(".bar");
+		b.animate({
+			opacity:0
+		}, Photon.speed, function() {
+			setTimeout(() => b.remove(),Photon.speed)
 		})
 	}).change(function() {
 		if ($(this).val() != "") {
@@ -2305,17 +2367,7 @@ Photon.ready = Photon.reload = function() {
 		$(this).toggleClass("active");
 	})
 
-	$(".waves-ink").off("mousedown").bind("mousedown", function(e) {
-		$(this).children(".waves-ripple").fadeOut(500);
-		e.stopPropagation();
-		Waves.ripple(this, {
-			wait: 1e10
-		});
-	}).on("mouseup mouseleave", function() {
-		Waves.calm(this);
-	}).bind("touchstart", function(e) {
-		e.stopPropagation()
-	})
+	Photon.Waves.reload();
 
 	setTimeout(() => {
 		$(".tabs.auto").each(function() {
@@ -2377,29 +2429,20 @@ Photon.ready = Photon.reload = function() {
 
 }
 
-$(() => Photon.ready());
+$(() => {
+	!Photon.autoready && Photon.ready();
+})
 
-setInterval(function() {
-	if ($("footer").length == 1) {
-		if ($("body").height() < $(window).innerHeight() - $("footer").height()) {
-			$("footer").addClass("attached-to-bottom")
-		} else {
-			$("footer").removeClass("attached-to-bottom")
-		}
-	}
+Photon.loop = () => {
 	$(".card-image.parallax").each(function() {
 		var t = $(this).children("img")
 		var px = ($("html").scrollTop() - t.offset().top * .75) / 20;
 		t[0].style.transform = "scale(2) translateY(" + px + "px)";
 	});
-	Photon.updateTextFields();
+	requestAnimationFrame(Photon.loop);
+}
 
-	$(".collapsible").each(function(){
-		$(this).children().removeClass("adjact");
-		$(this).children(".active").prev("li").addClass("adjact");
-	})
-
-})
+Photon.loop();
 
 setInterval(() => Waves.ripple($(".waves-pulse"),{
 	wait:750
