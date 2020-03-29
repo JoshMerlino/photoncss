a2412eeedf46ac5d5407913cf4fb1ba5c529ef69
Josh Merlino <joshua.merlino@gmail.com>
2020-03-28 22:12:00 -0400

    add buttons

---
+++ modules/Button.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Button extends React.Component {
+
+	// Define default props
+	static defaultProps = {
+		variant: "raised",
+		color: "",
+		size: "default",
+		waves: true
+	}
+
+	// Serialize the props object into a series of classes
+	serialize() {
+		return [
+			"btn",
+			...(this.props.className || "").split(" "),
+			this.props.variant,
+			this.props.color,
+			this.props.size !== "default" ? this.props.size : "",
+			this.props.waves !== false ? `waves-effect${this.props.waves !== true ? ` waves-${this.props.waves.replace(/\s/g, " waves-")}` : ""}` : "",
+			this.props.disabled ? "disabled" : ""
+		].join(" ").replace(/\s\s/gm, " ");
+	}
+
+	// Render component
+	render() {
+		return (
+			<button className={this.serialize()} style={this.props.style || {}}>{this.props.children}</button>
+		);
+	}
+
+}
+
+export default Button;
