cbd0aa17cba153a8d7973f67a58fe7671efaf02d
Josh Merlino <joshua.merlino@gmail.com>
2020-04-02 01:44:10 -0400

    add Radio

---
+++ modules/Radio.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Photon from "photoncss";
+
+class Radio extends React.Component {
+
+	// Define default props
+	static defaultProps = {
+		color: "primary",
+		disabled: false,
+		waves: "ink"
+	}
+
+	// Serialize the props object into a series of classes
+	serialize() {
+		const { color, disabled, waves, ...rest } = this.props;
+		this.rest = rest;
+		return [
+			"radio",
+			...(rest.className || "").split(" "),
+			color,
+			disabled ? "disabled" : "",
+		].join(" ").replace(/\s+(?=\s)/g, "").trim();
+	}
+
+	// Render component
+	render() {
+		return (
+			<div className={this.serialize()}>
+				<input type="radio" {...this.rest}/>
+				{ this.props.label && <label htmlFor={this.props.id || Photon.guid()}>{this.props.label}</label> }
+				{ this.props.waves !== false && <div className={`waves-effect${this.props.waves !== true ? ` ${Photon.prefixColorQuery("waves", this.props.waves)}` : ""}`}></div>}
+			</div>
+		);
+	}
+
+}
+
+export { Radio };
