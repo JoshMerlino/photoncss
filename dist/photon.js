d1786e0fc20bf110daa604bbda8ad384f0b1e9c7
Josh Merlino <joshua.merlino@gmail.com>
2020-04-20 21:27:14 -0400

    update prefixColorQuery

--- dist/photon.js
+++ dist/photon.js
@@ -1087,7 +1087,32 @@ var Photon = {
   },
   // Converts string color like "green accent-2" to "prefix-green prefix-accent-2"
   prefixColorQuery: function prefixColorQuery(prefix, query) {
-    return "".concat(prefix, "-").concat(query.replace(/\s/g, " ".concat(prefix, "-").concat(query)));
+    query = query.split(" ");
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = query[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var q = _step.value;
+        query[query.indexOf(q)] = "".concat(prefix, "-").concat(q);
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+          _iterator["return"]();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+
+    return query.join(" ");
   },
   // Binds event listeners where needed
   reload: function reload() {
