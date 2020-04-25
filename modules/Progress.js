2c214e56755bb6857e7731f58b51e16c6cbce83c
Josh Merlino <joshua.merlino@gmail.com>
2020-04-25 19:03:13 -0400

    add Progress

---
+++ modules/Progress.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Photon from "photoncss";
+
+class Spinner extends React.Component {
+
+	// Define default props
+	static defaultProps = {
+		color: "primary",
+		size: 64,
+		width: 6
+	}
+
+	// Serialize the props object into a series of classes
+	serialize() {
+		const { color, size, width, ...rest } = this.props;
+		this.rest = rest;
+		return [
+			"spinner",
+			...(rest.className || "").split(" "),
+			color,
+		].join(" ").replace(/\s+(?=\s)/g, "").trim();
+	}
+
+	// Render component
+	render() {
+		return (
+			<div className={`spinner-wrapper ${this.props.size || ""}`}>
+				<svg className={this.serialize()} {...this.rest} width="65" height="65" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+					<circle className="path" fill="none" strokeWidth={this.props.width} strokeLinecap="round" cx="33" cy="33" r="30"></circle>
+				</svg>
+			</div>
+		);
+	}
+
+}
+
+export { Spinner };
