766ac218df2bdd880571ec94c27f702dee40b74d
Josh Merlino <joshua.merlino@gmail.com>
2021-03-07 12:31:03 -0500

    rewrite button as functional component

--- src/modules/Button.js
+++ src/modules/Button.js
@@ -1,39 +1,46 @@
 import React from "react";
-import Photon from "../src/js/photon.js";
+import { prefixColorQuery } from "../src/js/photon.js";
+import classnames from "classnames";
 
-class Button extends React.Component {
+/**
+ * Assign the project to an employee.
+ * @param {Object} props - JSX Props
+ * @param {string} props.className - Additional classnames to be appended to the root component
+ * @param {string} props.variant - Type of button to use
+ * @param {string} props.color - Color of the button, ColorQuery
+ * @param {string} props.textColor - Text color of the button, ColorQuery
+ * @param {string} props.size - Size class of the button, dense | large
+ * @param {boolean} props.disabled - Disable the button
+ * @param {string|boolean} props.waves - Waves, if false no waves, else color query
+ */
+function Button({
+	className 		= "",
+	children 		= null,
+	variant			= "raised",
+	color			= "",
+	textColor		= "",
+	size			= "",
+	disabled  		= false,
+	waves 	 		= true,
+	...props
+}) {
 
-	// Define default props
-	static defaultProps = {
-		variant: "raised",
-		color: "",
-		size: "default",
-		textColor: false,
-		waves: true,
-	}
-
-	// Serialize the props object into a series of classes
-	serialize() {
-		const { variant, color, size, textColor, waves, disabled, ...rest } = this.props;
-		this.rest = rest;
-		return [
-			"btn",
-			...(rest.className || "").split(" "),
-			variant,
-			color,
-			size !== "default" ? size : "",
-			textColor !== false ? Photon.prefixColorQuery("text", textColor) : "",
-			waves !== false ? `waves-effect${waves !== true ? ` ${Photon.prefixColorQuery("waves", waves)}` : ""}` : "",
-			disabled ? "disabled" : ""
-		].join(" ").replace(/\s+(?=\s)/g, "").trim();
-	}
+	// Get element class
+	const classes = classnames("btn", {
+		[variant]: variant !== "",
+		[color]: color !== "",
+		[prefixColorQuery("text", textColor)]: textColor !== "",
+		[size]: size !== "",
+		disabled,
+		["waves-effect"]: waves !== false,
+		[prefixColorQuery("waves", typeof waves === "string" ? waves : "")]: typeof waves === "string"
+	}, className).replace(/\s\s+/g, " ");
 
 	// Render component
-	render() {
-		return (
-			<button className={this.serialize()} {...this.rest}>{this.props.children}</button>
-		);
-	}
+	return (
+		<button className={classes} {...props}>{children}</button>
+	)
+
 
 }
 
