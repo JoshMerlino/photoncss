59957a9ba29ab7aec0e4ab505554ee3fe3f68ddb
Josh Merlino <joshua.merlino@gmail.com>
2021-03-07 12:42:37 -0500

    do the same with banners

--- modules/index.js
+++ modules/index.js
@@ -9,5 +9,25 @@ Object.defineProperty(exports, "Button", {
     return _Button.Button;
   }
 });
+Object.defineProperty(exports, "Banner", {
+  enumerable: true,
+  get: function get() {
+    return _Banner.Banner;
+  }
+});
+Object.defineProperty(exports, "BannerActions", {
+  enumerable: true,
+  get: function get() {
+    return _Banner.BannerActions;
+  }
+});
+Object.defineProperty(exports, "BannerContent", {
+  enumerable: true,
+  get: function get() {
+    return _Banner.BannerContent;
+  }
+});
 
 var _Button = require("./Button");
+
+var _Banner = require("./Banner");
