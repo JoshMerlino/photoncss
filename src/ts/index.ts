0c600bc4ffe16588c6ed3643ed2e8dacd878f8f8
Josh Merlino <joshua.merlino@gmail.com>
2021-03-27 10:49:40 -0400

    restart and add raised-buttons

---
+++ src/ts/index.ts
@@ -0,0 +1,13 @@
+import guid from "./util/guid";
+export { guid };
+
+import ThemeProvider from "./util/class/ThemeProvider";
+export { ThemeProvider };
+
+import"./mixin/waves.js";
+
+export default class Photon {
+
+	static guid = guid;
+
+}
---
+++ webpack.conf.cjs
@@ -0,0 +1,50 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	mode: "production",
+	entry: [
+		"./lib/js/index.js",
+		"./src/less/index.less",
+	],
+	output: {
+		path: __dirname + "/dist",
+		filename: "./photon.js"
+	},
+	module: {
+        rules: [ {
+            test: /\.js$/,
+            use: {
+                loader: "babel-loader"
+            }
+        }, {
+			test: /\.css/i,
+			use: [ { loader: MiniCssExtractPlugin.loader, options: { publicPath: "./" } }, "css-loader" ]
+        }, {
+			test: /\.less/i,
+			use: [ { loader: MiniCssExtractPlugin.loader, options: { publicPath: "./" } }, "css-loader", {
+				loader: "less-loader",
+				options: {
+				    lessOptions: {
+						plugins: [
+							require("less-plugin-glob")
+						]
+				    },
+				}
+			}]
+		}, {
+			test: /\.(woff|woff2|eot|ttf|otf)$/,
+			use: [ {
+				loader: "file-loader",
+				options: { name: "font/[contenthash].[ext]" }
+			} ]
+		}, {
+			test: /\.(txt|md|pem|raw)$/,
+			use: [ "raw-loader" ]
+		} ]
+    },
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "photon.css"
+		}),
+	]
+};
