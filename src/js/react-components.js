229e97186b8860d5f71afd258b9c180a0052394c
Josh Merlino <joshua.merlino@gmail.com>
2021-03-27 22:17:50 -0400

    Add button block sizes

--- src/js/react-components.js
+++ src/js/react-components.js
@@ -11,4 +11,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./components/Button"), exports);
+__exportStar(require("./components/Layout"), exports);
 //# sourceMappingURL=react-components.js.map
--- src/ts/components/Button.tsx
+++ src/ts/components/Button.tsx
@@ -3,8 +3,10 @@ import PropTypes, { InferProps } from "prop-types";
 import classnames from "classnames";
 import Waves from "../mixin/waves.js";
 
-export function Button({ children, color, variant, waves, className = "", ...props }: InferProps<typeof Button.propTypes>) : JSX.Element {
-	const classes = classnames("photon-btn", `variant-${variant}`, `color-${color}`, waves && "waves-effect", className);
+/* ****************************************** */
+
+export function Button({ children, color, display, variant, size, waves, className = "", ...props }: InferProps<typeof Button.propTypes>) : JSX.Element {
+	const classes = classnames("photon-btn", `variant-${variant}`, `display-${display}`, `color-${color}`, `size-${size}`, waves && "waves-effect", className);
 	if(waves) setImmediate(Waves.init);
 	return <button className={classes} {...props}>{ children }</button>;
 }
@@ -13,7 +15,9 @@ Button.propTypes = {
 	children: PropTypes.any,
 	className: PropTypes.string,
 	color: PropTypes.oneOf([ "none", "primary", "secondary" ]),
+	size: PropTypes.oneOf([ "normal", "dense", "large" ]),
 	variant: PropTypes.oneOf([ "contained", "flat", "outlined", "raised" ]),
+	display: PropTypes.oneOf([ "inline", "block" ]),
 	waves: PropTypes.bool
 };
 
@@ -21,5 +25,7 @@ Button.defaultProps = {
 	children: null,
 	color: "none",
 	variant: "contained",
+	size: "normal",
+	display: "inline",
 	waves: true
 };
