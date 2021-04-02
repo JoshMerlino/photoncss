82c2acf543cd331372f94e6b0235c0b73d21d31e
Josh Merlino <joshua.merlino@gmail.com>
2021-04-01 22:27:14 -0400

    Add typings for waves

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,22 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ThemeProvider = exports.guid = void 0;
+exports.guid = void 0;
+// Import submodules/helper functions
 var guid_1 = __importDefault(require("./util/guid"));
 exports.guid = guid_1.default;
-var ThemeProvider_1 = __importDefault(require("./util/class/ThemeProvider"));
-exports.ThemeProvider = ThemeProvider_1.default;
+// Import waves
 require("./util/Waves.js");
-__exportStar(require("./util/class/notify"), exports);
-var notify_1 = require("./util/class/notify");
+// Import active components
+var Drawer_1 = require("./util/class/Drawer");
+// Export Photon
 var Photon = /** @class */ (function () {
     function Photon() {
     }
     Photon.guid = guid_1.default;
-    Photon.notify = notify_1.notify;
+    Photon.Drawer = function (target) { return new Drawer_1.Drawer(target); };
     return Photon;
 }());
 exports.default = Photon;
