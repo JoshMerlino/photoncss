e0cd2ce473643f6de3807967c10f8a99a6c2d11a
Josh Merlino <joshua.merlino@gmail.com>
2021-04-01 22:56:21 -0400

    add Drawer

--- src/ts/react-components.ts
+++ src/ts/react-components.ts
@@ -1,8 +1,9 @@
 export * from "./components/Button";
 export * from "./components/Card";
+export * from "./components/Drawer";
 export * from "./components/Icon";
 export * from "./components/Layout";
 export * from "./components/Snackbar";
+export { ThemeProvider };
 
 import ThemeProvider from "./util/class/ThemeProvider";
-export { ThemeProvider };
