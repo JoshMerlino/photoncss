646441e85fa883468dcdcea910ac52fc4c82589d
Josh Merlino <joshua.merlino@gmail.com>
2021-04-01 22:57:01 -0400

    add ability to send partial themes to theme provider

--- src/js/util/class/ThemeProvider.js
+++ src/js/util/class/ThemeProvider.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
@@ -48,6 +59,7 @@ function ThemeProvider(_a) {
     // If invalid theme type
     else
         throw new Error("'" + typeof theme + "' is not a valid theme.");
+    finalTheme = __assign(__assign({}, require("../../../../theme/default.light.json")), finalTheme);
     // If it is the root theme
     if (global) {
         // Render CSS
