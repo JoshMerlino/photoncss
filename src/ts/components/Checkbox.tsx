4f82df8d9aafb81ce61916c15980aa9ec62d1d50
Josh Merlino <joshua.merlino@gmail.com>
2021-04-04 19:47:07 -0400

    hard stash

--- src/ts/components/Checkbox.tsx
+++ src/ts/components/Checkbox.tsx
@@ -2,18 +2,30 @@ import React from "react";
 import PropTypes, { InferProps } from "prop-types";
 import classnames from "classnames";
 import guid from "../util/guid";
+import $ from "jquery";
+import Waves from "../util/Waves";
 
 /* ****************************************** */
 
-export function Checkbox({ children, labelPosition, color, id: explicitId, className = "", ...props }: InferProps<typeof Checkbox.propTypes>) : JSX.Element {
-	const classes = classnames("photon-checkbox", `color-${color}`, className);
+export function Checkbox({ children, labelPosition, color, waves, id, className = "", ...props }: InferProps<typeof Checkbox.propTypes>) : JSX.Element {
+	const classes = classnames("photon-checkbox", `color-${color}`, `labelposition-${labelPosition}`, className);
 
-	const id = explicitId || guid();
+	id = id || guid();
+
+	/*
+
+	// Define elements
+	const wrapper = $(`#${id}`).parent();
+	const ripple = wrapper.children(".ripple")[0];
+	wrapper.on("mousedown", () => Waves.ripple(ripple, { duration: 1e6, ink: true }));
+
+	*/
 
 	return (
 		<div className={classes}>
 			{ labelPosition === "before" && children && <label htmlFor={id}>{ children }</label> }
 			<input tabIndex={0} type="checkbox" id={id} {...props}/>
+			{ waves && <div className={classnames("ripple", waves && "waves-effect waves-ink")}></div> }
 			{ labelPosition === "after" && children && <label htmlFor={id}>{ children }</label> }
 		</div>
 	);
@@ -25,10 +37,12 @@ Checkbox.propTypes = {
 	id: PropTypes.string,
 	color: PropTypes.oneOf([ "none", "primary", "secondary" ]),
 	labelPosition: PropTypes.oneOf([ "before", "after" ]),
+	waves: PropTypes.bool
 };
 
 Checkbox.defaultProps = {
 	children: null,
 	color: "none",
-	labelPosition: "after"
+	labelPosition: "after",
+	waves: true
 };
