0cf10645f863a9e26f6cbcd54749084e279997c9
Josh Merlino <joshua.merlino@gmail.com>
2021-04-05 19:13:19 -0400

    clean code

--- src/ts/util/object.ts
+++ src/ts/util/object.ts
@@ -1,5 +1,4 @@
 export const deepKeys = (t: any, path: string[] = []) : string[] => Object(t) === t ? Object.entries(t).flatMap(([ k, v ]) => deepKeys(v, [ ...path, k ])) : [ path.join(".") ];
-Object.defineProperty(Object, "deepKeys", { value: deepKeys, writable: false });
 
 export const deepProp = (object: any, propString: string) : any => {
 	let value = object;
@@ -10,4 +9,6 @@ export const deepProp = (object: any, propString: string) : any => {
 	}
     return value;
 };
+
 Object.defineProperty(Object, "deepProp", { value: deepProp, writable: false });
+Object.defineProperty(Object, "deepKeys", { value: deepKeys, writable: false });
