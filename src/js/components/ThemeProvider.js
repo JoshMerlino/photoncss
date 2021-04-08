1b7d097363c96492625783ff4c8ab8b55e2c58cc
Josh Merlino <joshua.merlino@gmail.com>
2021-04-08 13:59:36 -0400

    add rest of input types

--- src/js/components/ThemeProvider.js
+++ src/js/components/ThemeProvider.js
@@ -10,43 +10,32 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ThemeProvider = void 0;
-var react_1 = __importStar(require("react"));
+var react_1 = __importDefault(require("react"));
 var theme_1 = require("../util/theme");
 function ThemeProvider(_a) {
     var _b = _a.theme, theme = _b === void 0 ? "default.light" : _b, _c = _a.global, global = _c === void 0 ? false : _c, children = _a.children;
     // Initialize theme
     var finalTheme;
-    // If theme is Theme
-    if (typeof theme === "object")
-        finalTheme = theme;
-    // If theme is string
-    else if (typeof theme === "string")
-        finalTheme = require("../../../theme/" + theme + ".json");
-    // If invalid theme type
-    else
-        throw new Error("'" + typeof theme + "' is not a valid theme.");
-    finalTheme = __assign(__assign({}, require("../../../theme/default.light.json")), finalTheme);
+    try {
+        // If theme is Theme
+        if (typeof theme === "object")
+            finalTheme = theme;
+        // If theme is string
+        else if (typeof theme === "string")
+            finalTheme = require("../../../theme/" + theme + ".json");
+        // If invalid theme type
+        else
+            throw new Error("'" + typeof theme + "' is not a valid theme.");
+        finalTheme = __assign(__assign({}, require("../../../theme/default.light.json")), finalTheme);
+    }
+    catch (_d) {
+        finalTheme = require("../../../theme/default.light.json");
+    }
     // If it is the root theme
     if (global) {
         // Render CSS
@@ -54,7 +43,7 @@ function ThemeProvider(_a) {
         // Add variables to root
         Object.keys(style_1).map(function (key) { return document.documentElement.style.setProperty(key, style_1[key]); });
         // Render children
-        return react_1.default.createElement(react_1.Fragment, null, children);
+        return react_1.default.createElement(react_1.default.Fragment, null, children);
     }
     else {
         // Render CSS
