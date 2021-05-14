6e8f07d18de8cbe70f24b69d60c839014998da48
Josh Merlino <joshua.merlino@gmail.com>
2021-05-13 21:24:22 -0400

    Add footer

--- src/js/components/Dialog.js
+++ src/js/components/Dialog.js
@@ -25,7 +25,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DialogActions = exports.DialogTitle = exports.Dialog = void 0;
+exports.DialogBody = exports.DialogActions = exports.DialogTitle = exports.Dialog = void 0;
 var react_1 = __importDefault(require("react"));
 var prop_types_1 = __importDefault(require("prop-types"));
 var classnames_1 = __importDefault(require("classnames"));
@@ -86,5 +86,19 @@ DialogActions.defaultProps = {
     children: null,
     direction: "right",
     seperated: false
+};
+/* ****************************************** */
+function DialogBody(_a) {
+    var children = _a.children, _b = _a.className, className = _b === void 0 ? "" : _b, props = __rest(_a, ["children", "className"]);
+    var classes = classnames_1.default("dialog-body", className);
+    return react_1.default.createElement("div", __assign({ className: classes }, props), children);
+}
+exports.DialogBody = DialogBody;
+DialogBody.propTypes = {
+    children: prop_types_1.default.any,
+    className: prop_types_1.default.string
+};
+DialogBody.defaultProps = {
+    children: null
 };
 //# sourceMappingURL=Dialog.js.map
