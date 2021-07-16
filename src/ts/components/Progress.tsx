c3c39d5b8acbf5b66bc9c56efcf8b886b29875dd
Josh Merlino <joshua.merlino@gmail.com>
2021-07-16 16:27:38 -0400

    add Progress and IndeterminateProgress

--- src/ts/components/Progress.tsx
+++ src/ts/components/Progress.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import PropTypes, { InferProps } from "prop-types";
 import classnames from "classnames";
 
 /* ****************************************** */
 
-export function Spinner({ size, color, className = "", ...props }: InferProps<typeof Spinner.propTypes> & InferProps<any>): JSX.Element {
+export interface SpinnerProps {
+	className?: string;
+	color?: "none" | "primary" | "secondary";
+	size?: "small" | "normal" | "large";
+}
+export function Spinner({ size = "normal", color = "none", className = "", ...props }: SpinnerProps): JSX.Element {
 	const classes = classnames("photon-spinner", `color-${color}`, `size-${size}`, className);
 	return (
 		<svg className={classes} width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg" {...props}>
@@ -13,13 +17,35 @@ export function Spinner({ size, color, className = "", ...props }: InferProps<ty
 	);
 }
 
-Spinner.propTypes = {
-	className: PropTypes.any,
-	color: PropTypes.oneOf([ "none", "primary", "secondary" ]),
-	size: PropTypes.oneOf([ "small", "normal", "large" ])
-};
+/* ****************************************** */
 
-Spinner.defaultProps = {
-	color: "none",
-	size: "normal"
-};
+export interface IndeterminateProgressProps {
+	className?: string;
+	color?: "none" | "primary" | "secondary";
+}
+export function IndeterminateProgress({ color = "none", className = "", ...props }: IndeterminateProgressProps): JSX.Element {
+	const classes = classnames("photon-progress", `color-${color}`, className);
+	return (
+		<div className={classes} {...props}>
+			<div className="indeterminate"></div>
+		</div>
+	);
+}
+
+/* ****************************************** */
+
+export interface ProgressProps {
+	className?: string;
+	color?: "none" | "primary" | "secondary";
+	value?: number;
+	min?: number;
+	max?: number;
+}
+export function Progress({ max = 1, min = 0, value = 0.5, color = "none", className = "", ...props }: ProgressProps): JSX.Element {
+	const classes = classnames("photon-progress", `color-${color}`, className);
+	return (
+		<div className={classes} {...props}>
+			<div className="determinate" style={{ width: `${(max - min) * value / max}%` }}></div>
+		</div>
+	);
+}
