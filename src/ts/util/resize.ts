2d626aa9efc10718bc6c1989ce2307c624f5adf2
Josh Merlino <joshua.merlino@gmail.com>
2021-11-08 13:57:05 -0500

    make even better

--- src/ts/util/resize.ts
+++ src/ts/util/resize.ts
@@ -1,8 +1,11 @@
 import $ from "jquery";
 
 export default function resize(): void {
-	const height = $("main")[0].scrollHeight;
-	$("main").height(height);
+	const main = $("main");
+	const footer = $("footer");
+
+	main.height(main[0].scrollHeight);
+	main.css("min-height", window.innerHeight - footer[0].clientHeight - 20);
 }
 
 $(function render() {
