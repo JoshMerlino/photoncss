361fdf86c9fbdffb390130615c64e29f06651864
Josh Merlino <joshua.merlino@gmail.com>
2021-11-11 12:30:46 -0500

    add function to resize main height

--- src/ts/util/resize.ts
+++ src/ts/util/resize.ts
@@ -4,8 +4,12 @@ export default function resize(): void {
 	const main = $("main");
 	const footer = $("footer");
 
-	main.height(main[0].scrollHeight);
-	main.css("min-height", window.innerHeight - footer[0].clientHeight - 20);
+	if (main[0]) main.height(main[0].scrollHeight);
+	if (footer[0]) {
+		main.css("min-height", window.innerHeight - footer[0].clientHeight - 20);
+	} else {
+		main.css("min-height", window.innerHeight);
+	}
 }
 
 $(function render() {
