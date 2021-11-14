2d32301701e73cee2be6533aeb1ee68c875056e1
Josh Merlino <joshua.merlino@gmail.com>
2021-11-13 22:13:09 -0500

    update again

--- src/ts/util/resize.ts
+++ src/ts/util/resize.ts
@@ -4,7 +4,7 @@ export default function resize(): void {
 	const main = $("main");
 	const footer = $("footer");
 
-	if (main[0]) main.height(main[0].scrollHeight);
+	// ; if (main[0]) main.height(main[0].scrollHeight);
 	if (footer[0]) {
 		main.css("min-height", window.innerHeight - footer[0].clientHeight - 20);
 	} else {
